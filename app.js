24b4fd46a8ddcf616904ef204c93eff31e7efb08
Bob Dunn <rpdunn@gmail.com>
2017-06-06 17:01:59 -0600

    add ability to view character info on character page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var characters = require('./routes/characters');
+var character = require('./routes/character');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/characters', characters);
+app.use('/character', character);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/character.js
+++ routes/character.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var store = require('../store')
+
 /* GET Characters listing. */
 router.get('/:name', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('character', {character:store.people.byName[req.params.name]})
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
